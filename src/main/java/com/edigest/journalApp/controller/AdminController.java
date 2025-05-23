8b18cd179c1ccccfc1d0066131caa457574f38c7
shreyasinha <shreyasinha4131@gmail.com>
2025-05-23 14:24:26 +0530

    SpringBoot learnig Lombok and all

---
+++ src/main/java/com/edigest/journalApp/controller/AdminController.java
@@ -0,0 +1,34 @@
+package com.edigest.journalApp.controller;
+
+
+import com.edigest.journalApp.entity.Users;
+import com.edigest.journalApp.service.UserService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/all-users")
+    public ResponseEntity<?> getAllUsers(){
+        List<Users> all = userService.getAll();
+        if(all !=null && !all.isEmpty()){
+            return new ResponseEntity<>(all, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/create-admin-user")
+    public void createUser(@RequestBody Users user){
+        userService.saveAdmin(user);
+    }
+}
--- src/main/java/com/edigest/journalApp/service/UserService.java
+++ src/main/java/com/edigest/journalApp/service/UserService.java
@@ -30,6 +30,12 @@ public class UserService {
         userRepository.save(user);
     }
 
+    public void saveAdmin(Users user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRoles(Arrays.asList("USER","ADMIN"));
+        userRepository.save(user);
+    }
+
     public List<Users> getAll(){
         return userRepository.findAll();
     }
